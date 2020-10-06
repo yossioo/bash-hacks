3f667c6bc4bc168b4349b46769b1ff35efbc0aa7
Yossi O <yossiovcharik@gmail.com>
2020-10-06 08:42:37 +0300

    Clean aliases

--- aliases.sh
+++ aliases.sh
@@ -21,19 +21,9 @@ YELLOW_TXT='\e[93m'
 NC='\033[0m'
 
 
-ROS1_NAME='melodic'
-ROS2_NAME='dashing'
-
-
-WS_FILE=$HOME/.ros_ws_selected
-LAUNCH_FILE=$HOME/.latest_launch
-
 
 # Define aliases:
 #alias my_ip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
-#alias sab='source /home/yossi/angry_ws/src/angry_birds_ros/angry_birds_ros/aliases.bash; source /home/yossi/angry_ws/devel/setup.bash'
-#alias umc='unalias mc'
-#alias test_vision='roslaunch angry_vision test.launch'
 
 alias cac='catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release'
 alias delete='gio trash '
@@ -51,7 +41,6 @@ alias fff='ffmpeg -f v4l2 -list_formats all -i '
 # Aliases for system management and administration
 alias rsync='rsync -a -v -h --progress'
 alias r='rsync -a -v -h --progress'
-alias midc='/usr/bin/mc'
 alias saud='sudo apt update; alu'
 alias saug='sudo apt upgrade'
 alias saar='sudo apt autoremove'
@@ -68,25 +57,16 @@ alias p3='python3'
 alias proc='ps -ef | grep '
 alias ..='cd ..'
 alias whR='nmap -sn 10.0.0.0/24'
-alias wapt='ps aux | grep -i apt'
-#alias upd_desktop_shortcuts='ln -nsf ~/Dropbox/zis_iz_backup/DesktopLinks/*.desktop ~/Desktop/;'
-alias upd_dash_shortcuts='ln -nsf ~/Dropbox/zis_iz_backup/DesktopLinks/*.desktop ~/.local/share/applications/'
+alias whapt='ps aux | grep -i apt'
 
-alias wnv="watch -n 0.2 'lsusb  |  grep -i nvidia'"
-# alias wnv="watch -n 0.2 'DV=$(lsusb  |  grep -i nvidia); if [ -z $DV ]; then printf \"No NVidia device found.\n\"; else printf \"Found: $DV\n\"; fi'"
-# alias wnv="watch -n 0.2 'DV=$(lsusb  |  grep -i nvidia); if [ -z $DV ]; then printf \"nothing\"; else printf  \"$DV\"; fi'"
-# alias wnv="watch -n 0.2 'DV=$(lsusb  |  grep -i nvidia); printf $DV'"
+alias wnv="watch -n 0.2 'DV=$(lsusb  |  grep -i nvidia); if [ -z $DV ]; then printf \"No NVidia device found.\n\"; else printf \"Found: $DV\n\"; fi'"
 alias sb='source ~/.bashrc'
 alias tso='xset dpms force off'
-#alias p="php -r 'echo \"Your PHP installation is working fine.\n\";'"
-alias sa='export PATH="/home/yossi/Apps/anaconda3/bin:$PATH"'
 alias kp='killall plasmashell; kstart plasmashell; exit'
-#alias wv='ls /dev/video*'
+alias wv='ls /dev/video*'
 alias pg='ping 8.8.8.8'
 alias srw='sudo grub-reboot 2 && reboot'
-#alias start_postgress="/usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile start"
-#alias start_postgress="/usr/lib/postgresql/9.5/bin/pg_ctl -D /var/lib/postgresql/9.5/main -l logfile start"
-#
+
 export EDITOR='nano -w'
 
 # Logout after X seconds
@@ -105,10 +85,8 @@ fi
 shopt -s histappend
 shopt -s cmdhist
 #export PROMPT_COMMAND='history -a'
-export HISTFILESIZE=5000000000 #-1
-export HISTSIZE=5000000 # -1
+export HISTFILESIZE=-1
+export HISTSIZE=-1
 export HISTCONTROL=ignoredups:erasedups # ignoreboth
-export HISTIGNORE='l:ls:bg:fg:history:kp'
 export HISTTIMEFORMAT="[$(tput setaf 6)%F %T$(tput sgr0)]: " # colorful date
-# export HISTTIMEFORMAT='%F %T '
 
